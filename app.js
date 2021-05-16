cca4221b69a303f8d2032c125f069263a3f888b0
murtazaaylak <murtazaaylak@gmail.com>
2021-05-16 23:30:13 +0300

    Route folder created

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const whoami = require('./routes/api/whoami')
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('hi')
 });
+
+app.use('/api/whoami', whoami);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port: ${port}`) );

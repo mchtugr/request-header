82edf674cdc6f45d64484df465189f125b397448
murtazaaylak <murtazaaylak@gmail.com>
2021-05-17 00:01:57 +0300

    show ip address, language browser info

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const whoami = require('./routes/api/whoami')
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('hi')
--- routes/api/whoami.js
+++ routes/api/whoami.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const requestIp = require('request-ip');
 
 router.get('/test', (req, res) => {
     res.json({
         msg: 'Test succeded'
     })
 });
+
+router.get('/', (req, res) => {
+    const ipaddress = requestIp.getClientIp(req);     
+    const language = req.headers["accept-language"];
+    const software = req.headers["user-agent"];
+    res.json({
+        ipaddress, language, software
+    })
+})
 
 module.exports = router;

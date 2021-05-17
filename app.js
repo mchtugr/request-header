3cb69f87367d0b9384134ed224ccfe5e2b3413f5
murtazaaylak <murtazaaylak@gmail.com>
2021-05-17 17:56:32 +0300

    example output change
    port update for heroku

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 });
 
-app.use('/api/whoami', whoami);
 
-const port = process.env.PORT || 3000;
+
+app.use('/api/whoami', whoami);
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server started on port: ${port}`) );
